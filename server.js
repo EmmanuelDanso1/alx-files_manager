575eba3cc8648ed36bad904f3f1c28ce72bcfec5
buyekeobare <buyekeobare@gmail.com>
2024-12-12 00:35:19 +0300

    file updated

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
 app.use('/', routes);
