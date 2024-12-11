286e120f0b9c4ce64e97352415e785e1051a9c1e
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-12-10 19:53:07 -0800

    routes

---
+++ server.js
@@ -0,0 +1,13 @@
+// server to connect to the routes folder with index.js file
+
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', routes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
